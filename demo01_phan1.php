c14205f3dcbbb5eb4531bb9940831fda2233bc06
TrinhHuyenTrang <149575048+TrinhHuyenTrang@users.noreply.github.com>
2023-11-04 14:50:21 +0700

    add file  demo01_phan1.php

---
+++ demo01_phan1.php
@@ -0,0 +1,8 @@
+<?php 
+    echo "Buổi 1: Học PHP";
+    echo "<br>";
+    echo " I- PHP là gì ?";
+    echo "<br>";
+    echo "PHP là một ngôn ngữ lập trình phía máy chủ (server-side scripting language) phổ biến được sử dụng rộng rãi để phát triển các ứng dụng web động. ";
+    echo "<br>";
+?>
